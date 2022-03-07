177ad0ef8d6265623571a99b0bc1a6676582ed40
leandro <leaguts21@gmail.com>
2022-03-07 17:08:55 -0500

    primero

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import logi from '../images/lol.png'
+import './footer.css'
+
+export const Footer = () => {
+  return (
+    <div className='ffooter'>
+        <div >
+            <img src={logi} className='logi'/>
+            
+        </div>
+        <div>
+        
+        </div>
+    </div>
+  )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './navbar.css'
+import logo from '../images/lol.png'
+import cart from '../images/carrosk.png'
+
+export const Navbar = () => {
+return (
+    <div className='techo'>
+        <div className='lefth-div'>
+            <img src={logo} className='logo'/>
+            <div className='letras-div'>
+                <div className='letras-letras'><h5>Nosotros</h5></div>
+                <div className='letras-letras'><h5>Productos</h5></div>
+            </div>
+            
+        </div>
+        <div className='right-div'>
+            <img src={cart} className='cart-div'/>
+        </div>
+    </div>
+)
+}
